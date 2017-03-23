687f388bb5bdbfdfd3d1ea49574cce93ce546462
nitindev111 <nitindev54@yahoo.com>
2017-03-24 03:41:53 +0530

    app.set devserver

--- devServer.js
+++ devServer.js
@@ -5,8 +5,10 @@ const config = require('./webpack.config.dev');
 
 const app = express();
 const compiler = webpack(config);
-const port=process.env.PORT || 5000;
+//const port=process.env.PORT || 5000;
 const host = 'http://localhost';
+app.set('port', (process.env.PORT || 5000));
+
 //const port = process.env.npm_config_port ? process.env.npm_config_port : 3000;
 
 app.use(require('webpack-dev-middleware')(compiler, {
@@ -20,10 +22,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, 'localhost', (err) => {
+app.listen(app.get('port'), 'localhost', (err) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.info('==> Listening on port %s. Open up %s:%s/ in your browser.', port, host, port);
+  console.info('==> Listening on port %s. Open up %s:%s/ in your browser.', app.get('port'), host, app.get('port'));
 });
